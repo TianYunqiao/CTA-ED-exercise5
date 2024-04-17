c486bb1ecf445de0d222d3bb80e1f9016b06273d
Marion Lieutaud <72838935+MarionLieutaud@users.noreply.github.com>
2024-04-17 14:15:34 +0100

    feedback

---
+++ Feedback.R
@@ -0,0 +1,30 @@
+# Feedback: CTA exercise 5 (2024)
+
+# question 1
+# -----------------------------------------------------------
+# the code is fine - it would have made sense to change your variable name (you kept 'tocq' as if you were still working with the Tocqueville books)
+
+# question 2
+# -----------------------------------------------------------
+# again the code is fine, althoygh you didn't adjust the consistency analysis (you could have e.g. compared different chapters rather than different volumes) 
+# you didn't explain your choice of a number of topics, and your topics are not very convincing (they look quite similar in terms of keywords) 
+# crucially, there is no interpretation of the analyses or results
+
+# question 3
+# -----------------------------------------------------------
+# You correctly ran the PreText analyses but didn't interpret the outputs. Would have been worth checking the documentation for PreText
+# that would have helped you make sense of the output and interpret it.
+
+# overall code quality: 
+# -----------------------------------------------------------
+# Overall you re-used the exercise code pretty well and you explained some of your codes and decisions well
+# You also correctly knitted the doc (although you kept the useless example markdown plots at the beginning?)
+# ideally you need to adjust the code to your particular interests, not just re-run it exactly as the example
+# e.g. in this case you could have compared differnet books by Austen or different chapters
+
+# overall interpretation: 
+# -----------------------------------------------------------
+# You didn't include any interpretation; this is a non-negotiable for the final assignment. 
+# If you only run the code but do not explain what the analyses means for the text and how to interpret and learn from the results 
+# and the modelling, then your assignment will be failed... 
+# Make sure you focus a lot more on interpretation and analyses in your final assignment!
